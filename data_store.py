4a5b0320faaa0f7d135165628f4e7514ffcfc273
slava <slavash02@mail.ru>
2023-04-03 20:57:03 +0300

    git commit --amend

--- data_store.py
+++ data_store.py
@@ -1,10 +1,24 @@
+import sqlalchemy
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
+Session = sessionmaker(bind=create_engine)
+session = Session()
 metadata = MetaData()
 Base = declarative_base()
+
+DSN = 'postgresql://postgres:230796@localhost:5432/db_store'
+engine = sqlalchemy.create_engine(DSN)
+
+
+def create_tables(profile_id, worksheet_id):
+    profile_id = profile_id
+    worksheet_id = worksheet_id
+
+
+session.commit()
 
 
 class Viewed(Base):
@@ -12,3 +26,14 @@ class Viewed(Base):
 
     profile_id = sq.Column(sq.Integer, primary_key=True)
     worksheet_id = sq.Column(sq.Integer, unique=True)
+
+    def __str__(self):
+        return f'Viewed {self.id}:({self.profile_id}, {self.worksheet_id})'
+
+
+def create_table(create_engine):
+    Base.metadata.drop_all(create_engine)
+    Base.metadata.create_all(create_engine)
+
+
+session.commit()
