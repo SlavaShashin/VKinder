036d13ec26ee39b15d348af46057fcb3b22492fa
slava <slavash02@mail.ru>
2023-04-05 20:36:41 +0300

    git commit --amend

--- data_store.py
+++ data_store.py
@@ -1,39 +1,43 @@
 import sqlalchemy
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import create_engine, MetaData
+
 from sqlalchemy.orm import sessionmaker
 
-Session = sessionmaker(bind=create_engine)
+DSN = 'postgresql://postgres:230796@localhost:5432/data_store'
+engine = sqlalchemy.create_engine(DSN)
+
+if __name__ == 'main':
+    from sqlalchemy import create_engine, MetaData
+    create_engine(DSN)
+
+Session = sessionmaker(bind=engine)
 session = Session()
 metadata = MetaData()
 Base = declarative_base()
 
-DSN = 'postgresql://postgres:230796@localhost:5432/db_store'
-engine = sqlalchemy.create_engine(DSN)
+
+session.commit()
 
 
 def create_tables(profile_id, worksheet_id):
     profile_id = profile_id
     worksheet_id = worksheet_id
 
-
-session.commit()
+    profile_id = sq.Column(sq.Integer, primary_key=True)
+    worksheet_id = sq.Column(sq.Integer, unique=True)
 
 
 class Viewed(Base):
     __tablename__ = 'viewed'
 
-    profile_id = sq.Column(sq.Integer, primary_key=True)
-    worksheet_id = sq.Column(sq.Integer, unique=True)
-
     def __str__(self):
         return f'Viewed {self.id}:({self.profile_id}, {self.worksheet_id})'
 
 
-def create_table(create_engine):
-    Base.metadata.drop_all(create_engine)
-    Base.metadata.create_all(create_engine)
+def create_table(self):
+    Base.metadata.drop_all(self)
+    Base.metadata.create_all(self)
 
 
 session.commit()
