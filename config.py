011386da1f4b8f841ee9df72ca4f3e7ba3beeb4a
slava <slavash02@mail.ru>
2023-04-13 19:23:39 +0300

    git commit --amend

--- config.py
+++ config.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+comunity_token = 'vk1.a' \
+                 '.whAd34Jbh8UCE3niQ_nT36GyuuoqhmcUZRTombzzZhczw6VQ9EDdSjP9lWc5_' \
+                 'SQHXTEadljIesJ9BxyxGdkuMJ9xzaVzYyF8J3wACNhur0Ird2gpuATBK8qtlZN3CQvf6AIsLQdf9PGmNbzIAiY9R-2m2QpC-' \
+                 'KUzb2994n66N02YGoR9Yz5O_BWfj4l9mAtal4wPkhMwqEa-NMSZkii71Q&expires_in=0&user_id=746002652'
+acces_token = 'vk1.a.ZtlKpLCcFwvwLqJ4KOLYGp2mDaKR3pGuZj8BZaa64MX9Fv3PCTFK9qy4bJsPZ2kVVXPxuiyYFcXhr-' \
+              'gLTcSvqMDExixIh7w9sRw-rY2EHRrwxAQPRpgj7T1S2alZoQSfRjr0XUe6QDU0s_' \
+              '-Odpt7iLspHNkVq37z4OjYLjCTOsHTKtH31mxKq01aMeAHqrjpiDYmx8R02_kKxUrD74qDEw'
+db_url_object = "postgresql://postgres:230796@localhost:5432/data_store"
