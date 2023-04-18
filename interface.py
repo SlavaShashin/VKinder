9886f94b56ebf858526c20ac19bf0dd344ca9eef
slava <slavash02@mail.ru>
2023-04-18 19:44:54 +0300

    git commit --amend

---
+++ interface.py
@@ -0,0 +1,34 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+
+from config import acces_token, comunity_token
+
+
+class BotInterface:
+
+    def __init__(self, token):
+        self.bot = vk_api.VkApi(token=comunity_token)
+
+    def message_send(self, user_id, message):
+        self.bot.method('messages.send',
+                        {'user_id': user_id,
+                         'message': message,
+                         'random_id': get_random_id()
+                         }
+                        )
+
+    def handler(self):
+        longpoll = VkLongPoll(self.bot)
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                if event.text.lower() == 'привет':
+                    self.message_send(event.user_id, 'хай')
+                elif event.text.lower() == 'поиск':
+                    self.message_send(event.user_id, 'введите имя')
+                elif event.text.lower() == 'далее':
+                    self.message_send(event.user_id, 'дальнейший поиск')
+                else:
+                    self.message_send(event.user_id, 'неизвестная команда')
+
+
