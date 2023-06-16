1cdce0ac51df7c1e44186821ddcacf27b2db2b97
slava <slavash02@mail.ru>
2023-06-16 16:06:45 +0300

    git commit

--- core.py
+++ core.py
@@ -1,81 +1,96 @@
+from datetime import datetime
+
 import vk_api
+
 from config import acces_token
-from vk_api.exceptions import ApiError
-
-token = acces_token
 
 
-class VkTools:
-    def __init__(self, token):
-        self.ext_api = vk_api.VkApi(token=token)
+class VkTools():
+    def __init__(self, acces_token):
+        self.api = vk_api.VkApi(token=acces_token)
 
     def get_profile_info(self, user_id):
 
+        info, = self.api.method('users.get',
+                                {'user_id': user_id,
+                                 'fields': 'city,bdate,sex,relation,home_town'
+                                 }
+                                )
+        user_info = {'name': info['first_name'] + ' ' + info['last_name'],
+                     'id': info['id'],
+                     'bdate': info['bdate'] if 'bdate' in info else None,
+                     'home_town': info['home_town'],
+                     'sex': info['sex'],
+                     'city': info['city']['id']
+                     }
+        return user_info
+
+    def serch_users(self, params):
+
+        sex = 1 if params['sex'] == 2 else 2
+        city = params['city']
+        curent_year = datetime.now().year
+        user_year = int(params['bdate'].split('.')[2])
+        age = curent_year - user_year
+        age_from = age - 5
+        age_to = age + 5
+
+        users = self.api.method('users.search',
+                                {'count': 10,
+                                 'offset': 0,
+                                 'age_from': age_from,
+                                 'age_to': age_to,
+                                 'sex': sex,
+                                 'city': city,
+                                 'status': 6,
+                                 'is_closed': False
+                                 }
+                                )
         try:
-            info = self.ext_api.method('users.get',
-                                       {'user_id': user_id,
-                                        'fields': 'bdate,city,sex,relation'
-                                        }
-                                       )
-        except ApiError:
-            return
+            users = users['items']
+        except KeyError:
+            return []
 
-        return info
+        res = []
 
-    def user_search(self, city_id, age_from, age_to, sex, relation, offset=None):
+        for user in users:
+            if user['is_closed'] == False:
+                res.append({'id': user['id'],
+                            'name': user['first_name'] + ' ' + user['last_name']
+                            }
+                           )
 
-        try:
-            profiles = self.ext_api.method('users.search',
-                                           {'city_id': city_id,
-                                            'age_from': age_from,
-                                            'age_to': age_to,
-                                            'sex': sex,
-                                            'relation': relation,
-                                            'count': 30,
-                                            'offset': offset
-                                            }
-                                           )
-        except ApiError:
-            return
+        return res
 
-        profiles = profiles['items']
-
-        result = []
-        for profile in profiles:
-            if not profile['is_closed']:
-                result.append({'name': profile['first_name'] + ' ' + profile['last_name'],
-                               'id': profile['id']
-                               })
-
-        return result
-
-    def photos_get(self, user_id):
-        photos = self.ext_api.method('photos.get',
-                                     {'album_id': 'profile',
-                                      'owner_id': user_id
-                                      }
-                                     )
-
+    def get_photos(self, user_id):
+        photos = self.api.method('photos.get',
+                                 {'user_id': user_id,
+                                  'album_id': 'profile',
+                                  'extended': 1
+                                  }
+                                 )
         try:
             photos = photos['items']
         except KeyError:
-            return
+            return []
 
-        result = []
-        for num, photo in enumerate(photos):
-            result.append({'owner_id': photo['owner_id'],
-                           'id': photo['id']
-                           })
+        res = []
 
-            if num == result.pop(2):
-                break
-        return result
+        for photo in photos:
+            res.append({'owner_id': photo['owner_id'],
+                        'id': photo['id'],
+                        'likes': photo['likes']['count'],
+                        'comments': photo['comments']['count'],
+                        }
+                       )
+
+        res.sort(key=lambda x: x['likes'] + x['comments'] * 10, reverse=True)
+
+        return res
 
 
-# if __name__ == 'main':
-#     tools = VkTools(acces_token)
-#     # print(tools.get_profile_info())
-#     # profiles = tools.user_search(1, 20, 40, 1)
-#     # print(profiles)
-#     photos = tools.photos_get()
-#     print(photos)
+if __name__ == '__main__':
+    bot = VkTools(acces_token)
+    params = bot.get_profile_info(789657038)
+    users = bot.serch_users(params)
+    print(bot.get_photos(users[2]['id']))
--- data_store.py
+++ data_store.py
@@ -1,97 +1,33 @@
-import sqlalchemy
+# импорты
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import IntegrityError, InvalidRequestError
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import Session
 
-DSN = 'db_url_object'
-engine = sqlalchemy.create_engine(DSN)
 
-if __name__ == 'main':
-    from sqlalchemy import create_engine, MetaData
-
-    create_engine(DSN)
-
-engine = create_engine(DSN)
-Session = sessionmaker(bind=engine)
-session = Session()
+# схема БД
 metadata = MetaData()
 Base = declarative_base()
 
-
-def create_table(self):
-    Base.metadata.drop_all(self)
-    Base.metadata.create_all(self)
-
-
-session.commit()
-
-
-class User(Base):
-    __tablename__ = 'user'
-    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
-    vk_id = sq.Column(sq.Integer, unique=True)
-
-
-class Photos(Base):
-    __tablename__ = 'photos'
-    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
-    link_photo = sq.Column(sq.String)
-    count_likes = sq.Column(sq.Integer)
-    id_dating_user = sq.Column(sq.Integer, sq.ForeignKey('dating_user.id', ondelete='CASCADE'))
-
-
-class SelectedProfiles(Base):
-    __tablename__ = 'selected_profiles'
-    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
-    vk_id = sq.Column(sq.Integer, unique=True)
-    first_name = sq.Column(sq.String)
-    second_name = sq.Column(sq.String)
-    city = sq.Column(sq.String)
-    link = sq.Column(sq.String)
-    id_user = sq.Column(sq.Integer, sq.ForeignKey('user.id', ondelete='CASCADE'))
-
-
 class Viewed(Base):
     __tablename__ = 'viewed'
-
     profile_id = sq.Column(sq.Integer, primary_key=True)
-    worksheet_id = sq.Column(sq.Integer, unique=True)
-
-    def __str__(self):
-        return f'Viewed {self.id}:({self.profile_id}, {self.worksheet_id})'
-
-    def add_db(self, profile_id, worksheet_id):
-        to_bd = Viewed(profile_id, worksheet_id)
-        session.add(to_bd)
-        return to_bd
-
-    def extract_db(self):
-        from_db = session.query(Viewed).filter(Viewed.profile_id == 123).all()
-        session.commit(self)
-        return from_db
+    worksheet_id = sq.Column(sq.Integer, primary_key=True)
 
 
-class Functions(Base):
-    def check_db(self):
-        current_user_id = session.query(User).filter_by(vk_id=self).first()
-        return current_user_id
+# добавление записи в бд
 
-    def check_db_user(self):
-        dating_user = session.query(SelectedProfiles).filter_by(
-            vk_id=self).first()
-        blocked_user = session.query(SelectedProfiles).filter_by(
-            vk_id=self).first()
-        return dating_user, blocked_user
+engine = create_engine(db_url_object)
+Base.metadata.create_all(engine)
+with Session(engine) as session:
+    to_bd = Viewed(profile_id=1, worksheet_id=1)
+    session.add(to_bd)
+    session.commit()
 
-    def register_user(self):
-        try:
-            new_user = User(vk_id=self)
-            session.add(new_user)
-            session.commit()
-            return True
-        except (IntegrityError, InvalidRequestError):
-            return False
+# извлечение записей из БД
 
-
-session.commit()
+engine = create_engine(db_url_object)
+with Session(engine) as session:
+    from_bd = session.query(Viewed).filter(Viewed.profile_id==1).all()
+    for item in from_bd:
+        print(item.worksheet_id)
