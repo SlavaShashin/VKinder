518e2abcaf31d249cbf13d211b84d74272fdbeea
slava <slavash02@mail.ru>
2023-06-30 18:19:32 +0300

    git commit

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-comunity_token = 'vk1.a.h_Q1D4E_FVFvtp8f5GdHANQW-RSxLtrate6HHYR0Lzf9z_RH-9xg1cwn-qhAFeqARtzE3bbaSwL4TvGOVhJE82wXKxymc' \
-                 'Mwg-S--k6R-ZdtG7bRJdluwu7x2IlIlbiE_WOBNx3lNq4_A3R3Nqs9PmpRr3qrRQxgKKnQhJfrJkHEtbeZzLOYeo0VhLMu1KbjG' \
-                 'f4DdZ8_O1Jlhv8upnp6y7g'
-acces_token = 'vk1.a.N5kJbF73_qz1VyvVtPmyOHHTlnWLPngqzDwFjGrOxvtGcT-TaqINTB8juoNjoMpVL8pPZzYw8fWBlyCmgY6GRuS9GD4auuAO' \
-              'f2ucQAwwqCVH90gp3n-QoVR1Ebnli6MD3X9uOsNKR1fdNCsPPQhE_2TNOWhXFUJinSWjoTYe8tu_QOlazMqDU125LOwxZ5FZVWzZ1T' \
-              'MB7qkZKhA3hrF17g'
-db_url = 'postgresql://postgres:230796@localhost:5432/data_store'
+comunity_token = '...'
+acces_token = '...'
+db_url = '...'
