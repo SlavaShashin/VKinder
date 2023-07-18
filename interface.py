548fb412c15085b0b0dbdd9bcd1b6a174d67f50b
slava <slavash02@mail.ru>
2023-07-18 20:44:36 +0300

    git commit

--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 import psycopg2
 import vk_api
 from psycopg2 import Error
+from sqlalchemy import event
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
@@ -11,11 +12,8 @@ from core import VkTools
 class BotInterface:
     def __init__(self, comunity_token, acces_token):
         self.interface = vk_api.VkApi(token=comunity_token)
-        self.longpoll = VkLongPoll(self.interface)
         self.api = VkTools(acces_token)
-        self.params = {}
-        self.worksheets = []
-        self.offset = 0
+        self.params = None
 
     def message_send(self, user_id, message, attachment=None):
         self.interface.method('messages.send',
@@ -35,10 +33,12 @@ class BotInterface:
 
                 if command == 'привет':
                     self.params = self.api.get_profile_info(event.user_id)
-                    self.message_send(event.user_id, f'Приветствую тебя, {self.params["name"]}')
+                    self.message_send(event.user_id, f'здравствуй {self.params["name"]}')
                 elif command == 'поиск':
-                    users = self.api.search_users(self.params)
+                    users = self.api.serch_users(self.params)
                     user = users.pop()
+
+                    photos_user = self.api.get_photos(user['id'])
 
                     if self.worksheets:
                         worksheets = self.worksheets.pop()
@@ -47,28 +47,14 @@ class BotInterface:
                         self.message_send(event.user_id, f'имя: {worksheets["name"]} '
                                                          f'ссылка: vk.com/{worksheets["id"]}'),
                         attachment = ''
-
-                        for num, photo in enumerate(photos_user):
-                            attachment += f'photo{photo["owner_id"]}_{photo["id"]},'
-                        else:
-                            self.worksheets = self.api.search_worksheet(self.params, self.offset)
-                            worksheet = self.worksheets.pop()
-                            photos_user = self.api.get_photos(worksheet['id'])
-
-                            self.message_send(event.user_id, f'имя: {worksheet["name"]} '
-                                                             f'ссылка: vk.com/{worksheet["id"]}'),
-                            attachment = ''
-
-                            for num, photo in enumerate(photos_user):
-                                attachment += f'photo{photo["owner_id"]}_{photo["id"]},'
-                                if num == 2:
-                                    break
-
-                                self.message_send(event.user_id,
-                                                  f'познакомтесь: {event.user_id["name"]}',
-                                                  attachment=attachment
-                                                  )
-                            self.offset += 10
+                    for num, photo in enumerate(photos_user):
+                        attachment += f'photo{photo["owner_id"]}_{photo["id"]}'
+                        if num == 2:
+                            break
+                    self.message_send(event.user_id,
+                                      f'Встречайте {user["name"]}',
+                                      attachment=attachment
+                                      )
 
                     try:
                         connection = psycopg2.connect(db_url)
@@ -93,21 +79,21 @@ class BotInterface:
                         session.add(dating_user)
                     elif user_answer == '2':
                         black_list_item = db_url.BlackList(user_number['id'], user_number['first_name'],
-                                                            user_number['last_name'], user_number['bdate'],
-                                                            user_number['sex'], user_number['city'],
-                                                            photos_attachment, user_number['domain'], event.user_id)
+                                                           user_number['last_name'], user_number['bdate'],
+                                                           user_number['sex'], user_number['city'],
+                                                           photos_attachment, user_number['domain'], event.user_id)
                         session.add(black_list_item)
                     elif user_answer == '3':
                         self.write_msg(user_id=event.user_id,
-                                        message=dictionaries_vk.options_messages['users_end_search'])
+                                       message=dictionaries_vk.options_messages['users_end_search'])
                         return False
                     session.commit()
-
-            elif command == 'пока':
-                self.message_send(event.user_id, 'пока')
-            else:
-                self.message_send(event.user_id, 'неизвестная команда')
+                elif command == 'пока':
+                    self.message_send(event.user_id, 'пока')
+                else:
+                    self.message_send(event.user_id, 'команда не опознана')
 
 
 if __name__ == '__main__':
     bot = BotInterface(comunity_token, acces_token)
+    print(bot)
