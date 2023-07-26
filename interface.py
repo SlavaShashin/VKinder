e12159954631b3cb6a5cadc1937eff6f13f389cd
slava <slavash02@mail.ru>
2023-07-26 17:36:04 +0300

    git commit

--- interface.py
+++ interface.py
@@ -3,7 +3,6 @@ from psycopg2 import Error
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
-import data_store
 from config import comunity_token, acces_token
 from core import VkTools
 from data_store import Viewed
@@ -13,6 +12,7 @@ class BotInterface:
     def __init__(self, comunity_token, acces_token):
         self.interface = vk_api.VkApi(token=comunity_token)
         self.api = VkTools(acces_token)
+        self.data_store = Viewed()
         self.params = None
 
     def message_send(self, user_id, message, attachment=None):
@@ -25,68 +25,51 @@ class BotInterface:
                               )
 
     def worksheets(self):
+        global attachment
         longpoll = VkLongPoll(self.interface)
 
-        try:
-            for event in longpoll.listen():
-                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                    command = event.text.lower()
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                command = event.text.lower()
 
-                    if command == 'привет':
-                        self.params = self.api.get_profile_info(event.user_id)
-                        self.message_send(event.user_id, f'здравствуй {self.params["name"]}')
-                    elif command == 'поиск':
-                        users = self.api.search_users(self.params)
-                        user = users.pop()
+                if command == 'привет':
+                    self.params = self.api.get_profile_info(event.user_id)
+                    self.message_send(event.user_id, f'здравствуй {self.params["name"]}')
+                elif command == 'поиск':
+                    users = self.api.search_users(self.params)
+                    user = users.pop()
 
-                        photos_user = self.api.get_photos(user['id'])
+                    photos_user = self.api.get_photos(user['id'])
 
-                        if self.worksheets:
-                            worksheets = self.worksheets.pop()
-                            photos_user = self.api.get_photos(worksheets['id'])
+                    if self.worksheets:
+                        worksheets = self.worksheets.pop()
+                        photos_user = self.api.get_photos(worksheets['id'])
 
-                            self.message_send(event.user_id, f'имя: {worksheets["name"]} '
-                                                             f'ссылка: vk.com/{worksheets["id"]}'),
-                            attachment = ''
-                        for num, photo in enumerate(photos_user):
-                            attachment += f'photo{photo["owner_id"]}_{photo["id"]}'
-                            if num == 2:
-                                break
-                        self.message_send(event.user_id,
-                                          f'Встречайте {user["name"]}',
-                                          attachment=attachment
-                                          )
+                        self.message_send(event.user_id, f'имя: {worksheets["name"]} '
+                                                         f'ссылка: vk.com/{worksheets["id"]}'),
+                        attachment = ''
+                    for num, photo in enumerate(photos_user):
+                        attachment += f'photo{photo["owner_id"]}_{photo["id"]}'
+                        if num == 2:
+                            break
+                    self.message_send(event.user_id,
+                                      f'Встречайте {user["name"]}',
+                                      attachment=attachment
+                                      )
 
-        except (Exception, Error) as error:
-            print('Ошибка базы данных', error)
+                    try:
+                        Viewed.check_user == Viewed.check_user
+                    except (Exception, Error) as error:
+                        print('Ошибка базы данных', error)
 
-        photos_attachment = ",".join(photos_user)
-        user_answer = []
-
-        if user_answer == '1':
-            dating_user = data_store.Viewed(['id'], user['first_name'],
-                                            user['last_name'], user['bdate'],
-                                            user['sex'], user['city'], photos_attachment,
-                                            event.user_id)
-            return dating_user
-
-        elif user_answer == '2':
-            black_list_item = data_store.Viewed(user['id'], user['first_name'],
-                                                user['last_name'], user['bdate'],
-                                                user['sex'], user['city'],
-                                                photos_attachment, event.user_id)
-            return black_list_item
-
-        elif user_answer == '3':
-            self.worksheets()
-            return False
-
-        elif command == 'пока':
-            self.message_send(event.user_id, 'пока')
-
-        else:
-            self.message_send(event.user_id, 'команда не опознана')
+                elif command == 'пока':
+                    self.message_send(event.user_id, 'пока')
+                else:
+                    self.message_send(event.user_id, 'команда не опознана')
 
 
 if __name__ == '__main__':
-    bot = BotInterface(comunity_token, acces_token)
+    bot = VkTools(acces_token)
+    params = bot.get_profile_info(789657038)
+    users = bot.search_users(params)
+    print(bot.get_photos(users[2]['id']))
